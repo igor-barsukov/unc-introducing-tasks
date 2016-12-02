cc34f0600f6692803de013b4a9402fb9563ef7a1
ogulenko-valentin <ogulenkov@inbox.ru>
2016-12-02 06:21:16 +0300

    commit 1st lab

---
+++ ogulenko-valentin/Parser/src/parser/InformationRecipient.java
@@ -0,0 +1,35 @@
+package parser;
+
+import java.util.List;
+import java.util.Map;
+
+public interface InformationRecipient {
+
+    /**
+     * getListOfEmployeeOnProject have to show list of employees which work on specified project
+     * @param project name of the project
+     * @return list of employees which work on specified project
+     */
+    public List<String> getListOfEmployeeOnProject(String project);
+
+    /**
+     * getListOfManagersForEmployee returns the list of superior for specified employee
+     * @param empName name of the employee
+     * @return the list of managers for empName
+     */
+    public List<String> getListOfManagersForEmployee(String empName);
+
+    /**
+     * getListOfNotBusyEmployees returns the list of employees who don't have the project
+     * @return not busy employees
+     */
+    public List<String> getListOfNotBusyEmployees();
+
+    /**
+     * getListOfProjectsForCustomer returns the projects of the customer
+     * @param customerName name of the customer
+     * @return the projects of the customer
+     */
+    public List<String> getListOfProjectsForCustomer(String customerName);
+
+}
